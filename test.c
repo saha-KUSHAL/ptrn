bd5ad1c314a514317e7e7fa3667e30677a02bbb2
saha-KUSHAL <krbfxofficial@gmail.com>
2023-04-22 20:55:21 +0530

    Test.c added

---
+++ test.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include"src/triangle/triangle.h" //replace with the right header
+int main(void){
+  int line;
+  printf("\nEnter no of lines:");
+  scanf("%d",&line);
+  t_vertical(line); //replace the function with the test function
+}
