472a8fd155c5860c00b89b2142c4fdc3d61fbbea
saha-KUSHAL <krbfxofficial@gmail.com>
2023-04-24 07:54:35 +0530

    Triangle patterns added

---
+++ src/triangle/t_default.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "triangle.h"
+
+void t_default(int line){
+  printf("%d",line);
+}
--- src/triangle/triangle.h
+++ src/triangle/triangle.h
@@ -1,5 +1,6 @@
 #ifndef _TRIANGLE_H
 #define _TRIANGLE_H
+int triangle(char *,int);
 void t_default(int);
 void t_vertical(int);
 #endif // !_TRIANGLE_H_
