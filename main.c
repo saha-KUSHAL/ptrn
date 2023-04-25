be2c0c28d2160b8ebd62b89fe7612623b1d927c5
saha-KUSHAL <krbfxofficial@gmail.com>
2023-04-25 20:12:59 +0530

    t_default added

--- main.c
+++ main.c
@@ -5,6 +5,8 @@ int main(int argc, char *argv[]){
   if(strcmp(argv[1],"triangle")==0){
     if(strcmp(argv[2],"vertical")==0)
       triangle("vertical",atoi(argv[3]));
+    if(strcmp(argv[2],"default")==0)
+      triangle("default",atoi(argv[3]));
   }
   return 0;
 }
--- src/triangle/t_default.c
+++ src/triangle/t_default.c
@@ -2,5 +2,16 @@
 #include "triangle.h"
 
 void t_default(int line){
-  printf("\nt_default function called, line = %d",line);
+  int i,j,limit1=line,limit2=line-1;
+  for(i=0;i<line;i++){
+    for(j=0;j<limit1;j++){
+      if(j<limit2)
+        printf(" ");
+      else
+        printf("*");
+    }
+    printf("\n");
+    limit1++;
+    limit2--;
+  }
 }
