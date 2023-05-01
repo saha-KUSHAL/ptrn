d7007ae331c9eb770eb489df0a20fae0fa1de8cf
saha-KUSHAL <krbfxofficial@gmail.com>
2023-05-01 22:06:02 +0530

    Argument manager: initial implementation

---
+++ src/system/argmanager.h
@@ -0,0 +1,10 @@
+#ifndef _ARGMANAGER_H
+#define _ARGMANAGER_H
+
+extern int loc_arr[5];
+extern int opt_check=0;
+extern char shape[20];
+extern char style[20];
+extern char opt;
+extern int line;
+#endif
--- src/system/shape.c
+++ src/system/shape.c
@@ -1,23 +1,24 @@
 #include "system.h"
 #include <string.h>
+#include "argmanager.h"
 char * shape(int argc, char *argv[]){
   int i,count=0;
-  char shape[20];
-  for(i=0;i<argc;i++){
+  char temp_shape[20];
+  for(i=1;i<argc;i++){
     if(strcmp(argv[i],"triangle")==0){
-      strcpy(shape,argv[i]);
+      strcpy(temp_shape,argv[i]);
       count++;
     }
     else if(strcmp(argv[i],"square")==0){
-      strcpy(shape,argv[i]);
+      strcpy(temp_shape,argv[i]);
       count++;
     }
     else if(strcmp(argv[i],"pentagon")==0){
-      strcpy(shape,argv[i]);
+      strcpy(temp_shape,argv[i]);
       count++;
     }
     else if(strcmp(argv[i],"hexagon")==0){
-      strcpy(shape,argv[i]);
+      strcpy(temp_shape,argv[i]);
       count++;
     }
     else
@@ -25,6 +26,10 @@ char * shape(int argc, char *argv[]){
   }
   if(count >1)
     return "Too many argument";
-  else
-    return shape;
+  else{
+    loc_arr[0]=i;
+    opt_check=1;
+    strcpy(shape,temp_shape);
+    return "Success";
+  }
 }
