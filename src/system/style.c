f469c53c5a05989e2ff638483e98b5799f53722a
saha-KUSHAL <krbfxofficial@gmail.com>
2023-05-01 23:16:42 +0530

    Argument manager: Intial process

--- src/system/style.c
+++ src/system/style.c
@@ -1,22 +1,35 @@
 #include "system.h"
 #include <string.h>
+#include "argmanager.h"
 char *style(int argc,char *argv[]){
-  int i,count =0;
-  char style[10];
-  for(i=0;i<argc;i++){
-    if(strcmp(argv[i],"vertical")==0){
-      strcpy(style,argv[i]);
-      count ++;
+  int i,count =0,pos;
+  char temp_style[10];
+  if(opt_check==1){
+    for(i=1;i<argc;i++){
+      if(i==loc_arr[0])
+        continue;
+      if(strcmp(argv[i],"vertical")==0){
+        strcpy(temp_style,argv[i]);
+        pos=i;
+        count ++;
+      }
+      else if(strcmp(argv[i],"inverted")==0){
+        strcpy(temp_style,argv[i]);
+        pos=i;
+        count++;
+      }
+     else
+        return "no shape";
     }
-    else if(strcmp(argv[i],"inverted")==0){
-      strcpy(style,argv[i]);
-      count++;
-    }
-    else
-      return "no shape";
   }
-  if(count >1)
+  if(count >1){
     return "Too Many Arguments";
-  else
-    return shape;
+    opt_check=0;
+  }
+  else{
+    opt_check =1;
+    loc_arr[1]=pos;
+    strcpy(style,temp_style);
+    return "Success";
+  }
 }
