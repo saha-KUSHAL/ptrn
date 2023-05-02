d78af3b3710ec42adf6dba85ac0df903cf0411b6
saha-KUSHAL <krbfxofficial@gmail.com>
2023-05-02 12:03:34 +0530

    Argmanager: Clearing the clutter
    -nuked system.h
    
    -added documentation in argmanager.h

--- src/system/argmanager.h
+++ src/system/argmanager.h
@@ -1,8 +1,28 @@
 #ifndef _ARGMANAGER_H
 #define _ARGMANAGER_H
 
+/*
+ * Searching and storing fucntions. Functions searches in the argv array and 
+ * stores the found match to it's corresponding varible in this header.
+ */
+
+char *shape(int,char *[]);
+char *style(int,char *[]);
+char *opts(int,char *[]);
+int line(int,char *[]);
+
+/*
+ * loc_arr array is used to store the location of matched data from the Functions
+ * above.
+ * opt_check is used to check entry of the sequntial Functions, style,opts and line;
+ */
 extern int loc_arr[5];
 extern int opt_check=0;
+
+/*
+ * This variables are used store the matched values
+ */
+
 extern char shape[20];
 extern char style[20];
 extern char opt[3];
--- src/system/system.h
+++
@@ -1,8 +0,0 @@
-#ifndef _SYSTEM_H
-#define _SYSTEM_H
-
-char *shape(int,char *[]);  //detrmines shape (triangle,square...etc)
-char *style(int,char *[]);  //determines style of the shape (horizontal,inverted...etc)
-char *otps(int,char *[]);   //finds flags like -h -c and returns them in a char array
-char *shape(int,char *[]);
-#endif
