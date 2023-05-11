9546f150f5091c98344fb0bc18ba24699570444f
saha-KUSHAL <krbfxofficial@gmail.com>
2023-05-11 22:48:41 +0530

    Arg Manager:
    Added Master Funtion calling

--- src/argmanager/argmanager.c
+++ src/argmanager/argmanager.c
@@ -1,6 +1,12 @@
 #include "argmanger.h"
-int argmanger(){
+#include "src/errorlib/error_lib.h"
+int argmanger(int argc, char *argv[]){
+  int s_value=0; 
   /* checking number of arguments.*/
+  if(argc=1){
+    printf("\nusgae: ptrn [-h] [-c] [triangle] [square] [pentagon] [hexagon]");
+    return 0;
+  }
   if(argc<5){
     printf("\nptrn: Too Few arguments.\v see 'ptrn --help' for some guide.");
     return 0;
@@ -10,15 +16,15 @@ int argmanger(){
     return 0;
   }
   
-  shape();
-  style();
-  opt();
-  line();
+  shape(argc,argv);
+  style(argc,argv);
+  opt(argc,argv);
+  line(argc,argv);
   /*
    * Determining the shape and initializing values
    * according to it, for further use in switch case;
    */
-  int s_value=0;
+  
   if(strcmp(shape,"triangle")==0)
     s_value=1;
   else if(strcmp(shape,"square")==0)
@@ -27,5 +33,20 @@ int argmanger(){
       s_value=3;
   else if(strcmp(shape,"hexagon")==0)
       s_value=4;
-  sw
+   /*
+    * calling master functions. Here s_value is used for it's 
+    * code shortning
+    */
+
+  switch(s_value){
+    case 1:triangle(shape,line);
+      break;
+    case 2:
+      break;
+    case 3:
+      break;
+    case 4:
+      break;
+    default:
+  }
 }
