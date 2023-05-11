8218c3f125ccc26eada7feb92febcde5d35104c6
saha-KUSHAL <krbfxofficial@gmail.com>
2023-05-11 23:00:43 +0530

    Feature Update: Square

---
+++ src/square/sqaure.c
@@ -0,0 +1,10 @@
+#include <string.h>
+#include "square.h"
+int square(char syl[],int line){
+  if(strcmp(syl,"default")==0)
+    //call default
+  else if(strcmp(sql,horizontal)==0)
+    //call horizontal
+  else
+    return 0;
+}
