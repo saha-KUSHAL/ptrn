ab8988b4804514d023992efc1016ce989b720604
saha-KUSHAL <krbfxofficial@gmail.com>
2023-05-13 21:43:55 +0530

    Arg Manager: fixing some logics

--- src/argmanager/argmanager.c
+++ src/argmanager/argmanager.c
@@ -1,7 +1,7 @@
 #include "argmanger.h"
 #include "src/errorlib/error_lib.h"
 int argmanger(int argc, char *argv[]){
-  int s_value=0; 
+  int s_value=0, i;
   /* checking number of arguments.*/
   if(argc=1){
     printf("\nusgae: ptrn [-h] [-c] [triangle] [square] [pentagon] [hexagon]");
@@ -15,24 +15,36 @@ int argmanger(int argc, char *argv[]){
     printf("\nptrn: Too Many arguments.\v see 'ptrn --help' for some guide.");
     return 0;
   }
-  
+  /*
+   * If there is no shape found in arguments
+   * opt_check will be 1. Using this condition
+   * we are calling other remaining functions
+   */
   shape(argc,argv);
-  style(argc,argv);
-  opt(argc,argv);
-  line(argc,argv);
+  for(i=0;i<3 && opt_check != 1;i++){
+    switch (i) {
+      case 0: style(argc,argv);
+        break;
+      case 1: opt(argc,argv);
+        break;
+      case 2: line(argc,argv);
+        break;
+    }
+  }
   /*
    * Determining the shape and initializing values
    * according to it, for further use in switch case;
    */
-  
-  if(strcmp(shape,"triangle")==0)
-    s_value=1;
-  else if(strcmp(shape,"square")==0)
-      s_value=2;
-  else if(strcmp(shape,"pentagon")==0)
-      s_value=3;
-  else if(strcmp(shape,"hexagon")==0)
-      s_value=4;
+  if(opt_check != 1){ 
+    if(strcmp(shape,"triangle")==0)
+      s_value=1;
+    else if(strcmp(shape,"square")==0)
+        s_value=2;
+    else if(strcmp(shape,"pentagon")==0)
+        s_value=3;
+    else if(strcmp(shape,"hexagon")==0)
+        s_value=4;
+  }
    /*
     * calling master functions. Here s_value is used for it's 
     * code shortning
@@ -47,6 +59,6 @@ int argmanger(int argc, char *argv[]){
       break;
     case 4:
       break;
-    default:
+    default: return 
   }
 }
